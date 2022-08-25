950ce55f52ad02cb77d398e8b26f8214c4ee40e5
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-08-25 11:30:55 +0800

    Answer

--- java-printer/src/App.java
+++ java-printer/src/App.java
@@ -4,8 +4,8 @@ import Printer.Printer;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        System.out.println(new Printer().print(new Document("Green", "AlloWorl")).color);
-        System.out.println(new ColorPrinter().print(new Document("Green", "AlloWorl")).color);
+        Document document = new Document("Green", "AlloWorl");
+        System.out.println(new Printer().print(document).color);
+        System.out.println(new ColorPrinter().print(document).color);
     }
 }
